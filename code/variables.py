457844b6db8a48867db4262ad4576354b1c0501d
Jorge Calvo <jorge@europeanvalley.es>
2018-04-05 13:07:06 +0200

    Create variables.py

---
+++ code/variables.py
@@ -0,0 +1,10 @@
+#Ejercicio 1
+ano = input ("Escribe el año en el que has nacido")
+edad = 2017 - (int(ano))
+print (edad)
+
+#Ejercicio 2
+cateto1 = input ("cuanto mide uno de los catetetos (en cm)")
+cateto2 = input ("cuanto mide el otro cateto (en cm)")
+hipotenusa= ((int(cateto1)**2) + (int(cateto2)**2))**0.5
+print ("La hipotenusa mide "+ (str(hipotenusa)) + " centimetros")
