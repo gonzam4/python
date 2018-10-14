1d7aa91b1505d0f979cd4cc1a404c308c6173d4c
Jorge Calvo <jorge@europeanvalley.es>
2018-10-14 11:39:40 +0200

    Update numerical.py

--- beginner/numerical.py
+++ beginner/numerical.py
@@ -1,7 +1,11 @@
 troncales=input("Asiganturas Troncales: " )
-troncales=int(troncales)
+troncales=int(troncales) #convertimos troncales en numérica
 opt=input("Asiganturas Optativas: " )
-opt=int(opt)
-totales=troncales + opt
-totales=str(totales)
-print("El numero de asignaturas elegidas es: " + totales)
+opt=int(opt) #convertimos optativas en numérica
+totales=troncales + opt #las sumamos y totales se convierte en númerica
+totales=str(totales) #convertimos troncales en texto
+troncales=str(troncales) #convertimos optativas en texto
+opt=str(opt) #convertimos totales en texto
+print("Tienes " + troncales + " troncales elegidas")
+print("Tienes " + opt + " optativas elegidas")
+print("El numero de asignaturas totales elegidas es: " + totales)
