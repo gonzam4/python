e4966814793ca521c74330aa126372db731e016c
Jorge Calvo <jorge@europeanvalley.es>
2018-10-14 11:24:36 +0200

    Create numerical.py

---
+++ beginner/numerical.py
@@ -0,0 +1,7 @@
+troncales=input("Asiganturas Troncales: " )
+troncales=int(troncales)
+opt=input("Asiganturas Optativas: " )
+opt=int(opt)
+totales=troncales + opt
+totales=str(totales)
+print("El numero de asignaturas elegidas es: " + totales)
